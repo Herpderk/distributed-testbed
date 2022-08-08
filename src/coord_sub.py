792a6554efcd76a0df71cc60aed1f9e6fe29f517
Derek Fan <herpderkfanie@gmail.com>
2022-08-07 18:35:34 -0700

    converter between Float32MultiArray and numpy array

--- src/coord_sub.py
+++ src/coord_sub.py
@@ -7,17 +7,24 @@ class CoordinateSubscriber:
 
 
 	def __init__(self):
-		# initialize node
-		rospy.init_node('coordinate_subscriber')
 		# define robot/tracker id
 		tracker_id = 0
+		# initialize node
+		rospy.init_node('coord_sub' + str(tracker_id))
 		# topic to subscribe to, name of callback function
-		self.sub = rospy.Subscriber("robot" + str(tracker_id), Float32MultiArray, self.log_coord)
+		sub = rospy.Subscriber("robot" + str(tracker_id), Float32MultiArray, self.path_track)
+		# initialize controller
+		self.controller = LQR_PID(radius=64.5, width=46.5, height=93.0, v=300.0, spd_ctrl=True)
+		# iterate thru path with index
+		self.path_index = 0
+
 		rospy.spin()
 
 
-	def log_coord(self, new_coord):
-		rospy.loginfo(str(new_coord.data))
+	def path_track(self, pos):
+		rospy.loginfo(str(pos.data))
+		
+
 
 
 if __name__ == '__main__':
